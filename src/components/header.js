e65a67f4ed39fa4002eb7e0a28e58cf84b427814
Ux-Beast <mosendab2010@gmail.com>
2024-01-15 22:39:30 +0200

    added header

---
+++ src/components/header.js
@@ -0,0 +1,9 @@
+const Header = () =>(
+    <Header>
+        <div className="container">
+            <div className="h1">Headless Blog</div>
+        </div>
+    </Header>
+)
+
+export default Header;
